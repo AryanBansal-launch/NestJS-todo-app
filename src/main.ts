2dce8f872bf16df6ea5af751ffd0afe72e2487e1
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-28 17:20:05 +0530

    removed val pipe

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
-
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
   console.log(`🚀 Server running on http://localhost:${port}`);
