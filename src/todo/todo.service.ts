439f2842b01049c8966ce363ac9eb7223babd000
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-28 15:16:29 +0530

    added logic in service file

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,40 @@
 import { Injectable } from '@nestjs/common';
-
+import { Todo } from './schemas/todo.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { NotFoundException } from '@nestjs/common';
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(@InjectModel(Todo.name) private todoModel: Model<Todo>) {}
+
+  //function to get all todos
+  async getAll(): Promise<Todo[]> {
+    return await this.todoModel.find();
+  }
+
+  //get todo by id
+  async getById(id: string): Promise<Todo> {
+    const todo = await this.todoModel.findById(id);
+    if (!todo) throw new NotFoundException('Todo not found');
+    return todo;
+  }
+
+  //create todo
+  async create(title: String): Promise<Todo> {
+    return await this.todoModel.create({ title });
+  }
+
+  //update todo
+  async update(id: string, completed: boolean): Promise<Todo> {
+    const updatedTodo = await this.todoModel.findByIdAndUpdate(id, { completed }, { new: true }).exec();
+    if (!updatedTodo) throw new NotFoundException('Todo not found');
+    return updatedTodo;
+  }
+
+  //delete todo
+  async delete(id:String):Promise<Todo>{
+    const deltetdTodo=await this.todoModel.findByIdAndDelete(id);
+    if(!deltetdTodo)throw new NotFoundException('Todo not found');
+    return deltetdTodo;
+  }
+}
