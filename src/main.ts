3faaa262495e7075f980b3917bd1e1abc2548c0b
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-28 18:14:57 +0530

    final fix and working

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import helmet from 'helmet';
-
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
