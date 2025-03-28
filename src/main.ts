553184686d04482ae0ed997ce67aa41b0469f9c8
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-28 17:43:50 +0530

    added helmet and cors

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-
-  const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port);
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
