5df27c5d68b8735c17d0eb1b4a18ddcd59f43565
Aryan Bansal <aryan.bansal@contentstack.com>
2025-03-31 12:30:44 +0530

    started

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,13 @@ import { App } from 'supertest/types';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TodoModule } from './todo/todo.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), // Loads environment variables
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/todo-app'), TodoModule, 
+    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://localhost:27017/todo-app'),  
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/todo/dto/update-todo.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsBoolean, IsOptional } from 'class-validator';
-
-export class UpdateTodoDto {
-  @IsOptional()
-  @IsBoolean()
-  completed?: boolean;
-}
