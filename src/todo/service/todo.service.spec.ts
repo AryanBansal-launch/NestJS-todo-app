d6ce36adac8e04828c10973a4ba615d630722813
Aryan Bansal <aryan.bansal@contentstack.com>
2025-04-01 15:15:29 +0530

    adding controler test

--- src/todo/service/todo.service.spec.ts
+++ src/todo/service/todo.service.spec.ts
@@ -1,12 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { TodoService } from './service/todo.service';
+import { TodoService } from './todo.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 describe('TodoService', () => {
   let service: TodoService;
 
+  // Mock Mongoose Model
+  const mockTodoModel = {
+    create: jest.fn(),
+    find: jest.fn(),
+    findById: jest.fn(),
+    findByIdAndUpdate: jest.fn(),
+    findByIdAndDelete: jest.fn(),
+  };
+
+  // Mock ConfigService
+  const mockConfigService = {
+    get: jest.fn().mockImplementation((key: string) => {
+      const envs = {
+        'mongo.uri': 'mongodb://localhost:27017/test-db', 
+      };
+      return envs[key];
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodoService],
+      providers: [
+        TodoService,
+        {
+          provide: getModelToken('Todo'),
+          useValue: mockTodoModel, 
+        },
+        {
+          provide: ConfigService,
+          useValue: mockConfigService, 
+        },
+      ],
     }).compile();
 
     service = module.get<TodoService>(TodoService);
