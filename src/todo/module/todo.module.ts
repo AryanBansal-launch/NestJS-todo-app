28c1f24c18152c4dc57701018645475427753a07
Aryan Bansal <aryan.bansal@contentstack.com>
2025-04-04 14:32:14 +0530

    folder restructure

--- src/todo/module/todo.module.ts
+++ src/todo/module/todo.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TodoService } from './service/todo.service';
-import { TodoController } from './controllers/todo.controller';
+import { TodoService } from '../service/todo.service';
+import { TodoController } from '../controllers/todo.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { Todo } from './schema/todo.schema';
-import { TodoSchema } from './schema/todo.schema';
+import { Todo } from '../schema/todo.schema';
+import { TodoSchema } from '../schema/todo.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Todo.name, schema: TodoSchema }])],
