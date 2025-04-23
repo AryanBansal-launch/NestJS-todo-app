ffaa9341be7260b323380e77a1d1974e5a861b19
Aryan Bansal <aryan.bansal@contentstack.com>
2025-04-23 14:54:07 +0530

    independent tests in integration tests

--- src/todo/module/todo.module.spec.ts
+++ src/todo/module/todo.module.spec.ts
@@ -85,7 +85,7 @@
 //   });
 // });
 
-// test/todos/todo.e2e-spec.ts
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
