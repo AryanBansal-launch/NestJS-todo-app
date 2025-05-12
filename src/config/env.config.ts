7e25746127324e4b62850c8189c79d09b3aa8e9f
Aryan Bansal <aryan.bansal@contentstack.com>
2025-05-12 16:04:28 +0530

    initializing the setup and config the database

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,9 +1,9 @@
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 config();
 
 const envConfig = () => ({
-    database: process.env.DATABASE_URL || '',
-    PORT: parseInt(process.env.PORT || '3000',10)
-  });
-  
-  export default envConfig;
+  database: process.env.DATABASE_URL || '',
+  PORT: parseInt(process.env.PORT || '3000', 10),
+});
+
+export default envConfig;
