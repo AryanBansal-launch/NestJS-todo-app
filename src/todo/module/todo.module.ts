fee810da0d204a52c77fc2e5b2d6a43fcfa1298b
Aryan Bansal <aryan.bansal@contentstack.com>
2025-09-15 16:14:53 +0530

    prettier changes

--- src/todo/module/todo.module.ts
+++ src/todo/module/todo.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { TodoController } from '../controller/todo.controller';
 import { TodoService } from '../service/todo.service';
