19bb0edb5520cbc1786ecf6929f97462e3417767
Aryan Bansal <aryan.bansal@contentstack.com>
2025-09-15 16:19:09 +0530

    Bootstrapped fresh branch

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,11 @@
 import { App } from 'supertest/types';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TodoModule } from './todo/module/todo.module';
-import envConfig from './config/env.config';
-import { ConfigService } from '@nestjs/config';
-import { TodoService } from './todo/service/todo.service';
-import { TodoController } from './todo/controller/todo.controller';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [envConfig],
-      isGlobal: true,
-    }),
-    MongooseModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('database', ' '),
-      }),
-      inject: [ConfigService],
-    }),
-    TodoModule,
-  ],
-  controllers: [AppController, TodoController],
-  providers: [AppService, TodoService],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/config/env.config.ts
+++
@@ -1,9 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-const envConfig = () => ({
-  database: process.env.DATABASE_URL || '',
-  PORT: parseInt(process.env.PORT || '3000', 10),
-});
-
-export default envConfig;
